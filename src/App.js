8318935bc35f9ed0d107cb01cc363e770d80e8b3
Kayla Camacho <kcamacho@nami.org>
2021-03-18 14:25:27 -0400

    Cade, please help

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as FlexWebChat from "@twilio/flex-webchat-ui";
+import ReactDOM from 'react-dom'
 
 class App extends React.Component {
 
@@ -9,8 +10,25 @@ class App extends React.Component {
     super(props);
 
     const { configuration } = props;
+
+    FlexWebChat.MainHeader.defaultProps.showImage = false;
+
+    // FlexWebChat.MessagingCanvas.defaultProps.showTrayOnInactive = false;
+
     FlexWebChat.Manager.create(configuration)
-      .then(manager => this.setState({ manager }))
+      .then(manager => {
+        manager.strings.WelcomeMessage = "NAMI Helpline </br> <a target='_' href='http://www.google.com'>Webchat Terms of Service</a>";
+        manager.strings.PredefinedChatMessageAuthorName = "NAMI Helpline";
+        manager.strings.PredefinedChatMessageBody = "Thank you for contacting to the NAMI Helpline.  How can we help you today?  NOTE: If you are in crisis, please text CRISIS."
+
+        ReactDOM.render(
+            <FlexWebChat.ContextProvider manager={manager}>
+                <FlexWebChat.RootContainer />
+            </FlexWebChat.ContextProvider>,
+            document.getElementById("root")
+        );
+    })
+        //this.setState({ manager }))
       .catch(error => this.setState({ error }));
   }
 
