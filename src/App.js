c5b1931d0f683fc5ec19b3646c0276439abc8181
Kayla Camacho <kcamacho@nami.org>
2021-06-24 16:49:04 -0400

    buttons to help abandoned chat functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import * as FlexWebChat from "@twilio/flex-webchat-ui";
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import MinimizeButton from "./components/MinimizeButton"
+import CloseButton from "./components/CloseButton"
+import { AppConfig } from '@twilio/flex-webchat-ui';
 
 class App extends React.Component {
 
@@ -13,14 +16,34 @@ class App extends React.Component {
 
     FlexWebChat.MainHeader.defaultProps.showImage = false;
 
-    // FlexWebChat.MessagingCanvas.defaultProps.showTrayOnInactive = false;
+
+
+    FlexWebChat.MessageCanvasTray.defaultProps.showButton = false;
+    FlexWebChat.MessagingCanvas.defaultProps.memberDisplayOptions = {
+      yourDefaultName: 'You',
+      theirDefaultName: 'NAMI HelpLine',
+      yourFriendlyNameOverride: false,
+      theirFriendlyNameOverride: false,
+    };
+
+    FlexWebChat.Manager.create(configuration)
+    .then((manager) => {
+      // Add the EndChat component into FlexWebChat
+      FlexWebChat.MainHeader.Content.add(<MinimizeButton key="mimize-chat"  />, { sortOrder: -1, align: "end" });
+      FlexWebChat.MainHeader.Content.add(<CloseButton key="close-chat" runtimeDomain={AppConfig.current().runtimeDomain} manager={manager}/>, { sortOrder: -1, align: "end" });
+      FlexWebChat.MainHeader.Content.remove("close-button");
+
+      manager.strings.WelcomeMessage ="NAMI Helpline</br><a target='_' href='https://www.nami.org/Terms-of-Use/NAMI-HelpLine-Terms-of-Service'>Terms of Service</a>";
+      manager.strings.MessageCanvasTrayContent = '<h6>Thanks for chatting with us!</h6><p style="text-align:center;font-size:12px">Please remember to save any resources you received today. <br/> <br/> If you have any more questions, please reach out to us again.</p>';
+      manager.strings.PredefinedChatMessageBody = 'Welcome to the NAMI HelpLine.  If you are in crisis, text CRISIS.  Before you proceed, please review our terms of use, linked above. If you understand and agree to the terms of use, text GO';
+      manager.strings.PredefinedChatMessageAuthorName = 'NAMI HelpLine'
+
+      this.setState({ manager });
+    }).catch(error => this.setState({ error }));
 
     FlexWebChat.Manager.create(configuration)
       .then(manager => {
-        manager.strings.WelcomeMessage = "NAMI Helpline </br> <a target='_' href='http://www.google.com'>Webchat Terms of Service</a>";
-        manager.strings.PredefinedChatMessageAuthorName = "NAMI Helpline";
-        manager.strings.PredefinedChatMessageBody = "Thank you for contacting to the NAMI Helpline.  How can we help you today?  NOTE: If you are in crisis, please text CRISIS."
-
+                
         ReactDOM.render(
             <FlexWebChat.ContextProvider manager={manager}>
                 <FlexWebChat.RootContainer />
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'regenerator-runtime/runtime';
